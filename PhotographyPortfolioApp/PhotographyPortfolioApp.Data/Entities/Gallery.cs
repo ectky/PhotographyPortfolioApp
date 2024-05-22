bffd76252c209e72e93c7aa388e821d1fd7f34fb
unknown <krisi.lozeva@gmail.com>
2024-05-22 10:42:31 +0300

    Created entities and enums

--- PhotographyPortfolioApp/PhotographyPortfolioApp.Data/Entities/Gallery.cs
+++ PhotographyPortfolioApp/PhotographyPortfolioApp.Data/Entities/Gallery.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PhotographyPortfolioApp.Shared.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,6 @@ namespace PhotographyPortfolioApp.Data.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public int UserId { get; set; }
+        public GalleryType GalleryType { get; set; }
     }
 }
