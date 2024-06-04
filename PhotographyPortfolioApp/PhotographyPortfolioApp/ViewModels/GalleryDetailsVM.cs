ecb402c98d3a255c941e22dff1136b5efedefb18
unknown <krisi.lozeva@gmail.com>
2024-06-04 19:34:59 +0300

    Created View Models

---
+++ PhotographyPortfolioApp/PhotographyPortfolioApp/ViewModels/GalleryDetailsVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotographyPortfolioApp.ViewModels
+{
+    public class GalleryDetailsVM : BaseVM
+    {
+        public GalleryDetailsVM()
+        {
+            this.PhotoGalleries = new List<PhotoGalleryDetailsVM>();
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public UserDetailsVM User { get; set; }
+        public virtual List <PhotoGalleryDetailsVM> PhotoGalleries { get; set; }
+
+    }
+}
