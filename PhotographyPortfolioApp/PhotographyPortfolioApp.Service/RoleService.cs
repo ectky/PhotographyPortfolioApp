b91d06deb3d07482b275eecec3565098b830b21e
Krisi-23 <ganevakristina57@gmail.com>
2024-06-04 19:41:22 +0300

    Errors

---
+++ PhotographyPortfolioApp/PhotographyPortfolioApp.Service/RoleService.cs
@@ -0,0 +1,22 @@
+﻿using PhotographyPortfolioApp.Shared.Attributes;
+using PhotographyPortfolioApp.Shared.Dtos;
+using PhotographyPortfolioApp.Shared.Repos.Contracts;
+using PhotographyPortfolioApp.Shared.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotographyPortfolioApp.Services
+{
+    [AutoBind]
+    public class RoleService : BaseCrudService<RoleDto, IRoleRepository>, IRoleService
+    {
+        public RoleService(IRoleRepository repository) : base(repository)
+        {
+
+        }
+    }
+}
+
