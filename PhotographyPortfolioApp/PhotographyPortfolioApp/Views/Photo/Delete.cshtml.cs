f6720c6a03d1902ede6754a7a10d9d196b0fe925
unknown <krisi.lozeva@gmail.com>
2024-06-11 10:06:52 +0300

    Added views

---
+++ PhotographyPortfolioApp/PhotographyPortfolioApp/Views/Photo/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShelter.Views.Pet
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
